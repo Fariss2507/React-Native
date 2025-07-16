515d38830d92a15297698f50dbbccd0837d3269c
Fariss2507 <farissfaheem1907@gmail.com>
2025-07-16 21:26:47 +0500

    view, text, button

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSzie: 50}}> React Native </Text>
+      <Text style={{ fontSzie: 40}}> Hey everyone </Text>
+      <Text style={{ fontSzie: 30}}> Im learning  </Text>
+      <Button title="yahan press kro"/>
+    </View>
+  )
+}
+
+export default App
